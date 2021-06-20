71831ccdafd34e8ad128df26e0b3d469f034f154
Maycon <maycon.carlete@gmail.com>
2021-06-20 09:59:32 -0300

    chore: add jest, ts-jest, @types/jest config

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/index.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/infra/**'
+  ]
+};
