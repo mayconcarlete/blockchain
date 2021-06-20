59685c3998e7c3ed83533d1dc12d8b938b42f4ea
Maycon <maycon.carlete@gmail.com>
2021-06-20 16:12:17 -0300

    feat: ensure mine block is added in chain

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -1,15 +1,19 @@
 import {Block} from './types/block'
 import {MineBlock} from './services/mine-block/mine-block'
 import { AddBlock } from './interfaces/blockchain'
+import { Transaction } from './types/transaction'
 
 export class BlockChain implements AddBlock{
     constructor(
-        private chain:Block[],
+        private chain: Block[],
+        private readonly mineBlock: MineBlock
     ){}
-    addBlock(block: Block): Block {
-        const lastBlock = this.getLastBlock()   
-        return this.chain[0] 
+    addBlock(data: Transaction): Block {
+        const lastBlock = this.getLastBlock()
+        this.chain.push(this.mineBlock.mine(lastBlock, data))
+        return this.getLastBlock()
     }
+    
     getLastBlock():Block{
         return this.chain[this.chain.length - 1]
     }
--- src/interfaces/blockchain.ts
+++ src/interfaces/blockchain.ts
@@ -1,5 +1,6 @@
 import { Block } from "@src/types/block";
+import { Transaction } from "@src/types/transaction";
 
 export interface AddBlock{
-    addBlock(block:Block):Block
+    addBlock(data: Transaction):Block
 }
