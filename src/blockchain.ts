45fea0ded9d1163849583af08d9fb5c0c3c6c4de
Maycon <maycon.carlete@gmail.com>
2021-06-20 18:24:44 -0300

    feat: ensure BlockChain validate chain from another fork

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -2,11 +2,13 @@ import {Block} from './types/block'
 import {MineBlock} from './services/mine-block/mine-block'
 import { AddBlock } from './interfaces/blockchain'
 import { Transaction } from './types/transaction'
+import { ValidateChain } from './helpers/validate-chain'
 
 export class BlockChain implements AddBlock{
     constructor(
         private chain: Block[],
-        private readonly mineBlock: MineBlock
+        private readonly mineBlock: MineBlock,
+        private readonly validateChain: ValidateChain
     ){}
     addBlock(data: Transaction): Block {
         const lastBlock = this.getLastBlock()
@@ -16,5 +18,8 @@ export class BlockChain implements AddBlock{
     
     getLastBlock():Block{
         return this.chain[this.chain.length - 1]
+    }
+    isValidChain():boolean{
+        return this.validateChain.validate(this.chain)
     }
 }
